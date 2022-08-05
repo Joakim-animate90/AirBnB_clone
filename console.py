edbb2960f480c833c78d3838e1e354638c51d0aa
Joakim-animate90 <joakimbwire23@gmail.com>
2022-08-05 18:56:19 +0000

    [Add]:Addition of new feature all

--- console.py
+++ console.py
@@ -73,7 +73,20 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     del storage.all()[key]
                     storage.save()
-
+    def do_all(self, line):
+        """Prints all string representation of all instances.
+        """
+        if line != "":
+            words = line.split(' ')
+            if words[0] not in storage.classes():
+                print("** class doesn't exist **")
+            else:
+                l = [str(obj) for key, obj in storage.all().items()
+                     if type(obj).__name__ == words[0]]
+                print(l)
+        else:
+            l = [str(obj) for key, obj in storage.all().items()]
+            print(l)
 
 
 
