b53f21000c72f12773d40f272657846996914dd8
Joakim-animate90 <joakimbwire23@gmail.com>
2022-08-06 07:05:32 +0000

    [Add]: New feature update

--- console.py
+++ console.py
@@ -89,6 +89,54 @@ class HBNBCommand(cmd.Cmd):
             print(l)
 
 
+    def do_update(self, line):
+
+        """Updates an instance by adding or updating an attribute.
+        """
+        if line == "" or line is None:
+            print("** class name missing **")
+            return
+            
+            rex = r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
+            match = re.search(rex, line)
+            classname = match.group(1)
+            uid = match.group(2)
+            attribute = match.group(3)
+            value = match.group(4)
+            if not match:
+                print("** class name missing **")
+            elif classname not in storage.classes():
+                print("** class doesn't exist **")
+            elif uid is None:
+                print("** instance id missing **")
+            else:
+                key ="{}.{}".format(classname, uid)
+                if key not in storage.all():
+                    print("** no instance found **")
+                elif not attribute:
+                    print("** attribute name missing **")
+                elif not value:
+                    print("** value missing **")
+                else:
+                    cast = None
+                    if not re.search('^".*"$', value):
+                        if '.' in value:
+                            cast = float
+                        else:
+                            cast = int
+                    else:
+                        value = value.replace('"','')
+                        attributes = storage.all()[classname]
+                    if attribute in attributes:
+                        value = attributes[attribute](value)
+                    elif cast:
+                        try:
+                            value = cast(value)
+                            except ValueError:
+                                pass  # fine, stay a string then
+                    setattr(storage.all()[key], attribute, value)
+                    storage.all()[key].save()
+
 
 
 
